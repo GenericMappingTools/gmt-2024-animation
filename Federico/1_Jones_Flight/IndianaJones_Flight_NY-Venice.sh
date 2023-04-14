bd34188499d074b1111fb318ca3a457609597fb7
Federico Esteban <federico.esteban@gmail.com>
2023-04-14 08:46:42 -0300

    Update IndianaJones_Flight_NY-Venice.sh
    Thanks Paul.
    
    I made so extra modifications.

--- Federico/1_Jones_Flight/IndianaJones_Flight_NY-Venice.sh
+++ Federico/1_Jones_Flight/IndianaJones_Flight_NY-Venice.sh
@@ -7,24 +7,25 @@ gmt begin
 
 	# Dr. Jones stopover cities
 	cat <<- 'FILE' > cities.txt
-	-74.007	40.712	New York
+	-74.007	40.712	New York (USA)
 	-52.712	47.562	St Johns (Terranova)
 	-25.696	37.742	Sao Miguel (Azores)
-	-9.135	38.776	Lisboa (Portugal)
-	 12.342	45.503	Venecia (Italia)
+	-9.135	38.776	Lisbon (Portugal)
+	 12.342	45.503	Venice (Italy)
 	FILE
+    animation_duration=30  # in seconds
     dist_to_Venice=$(gmt mapproject -G+uk cities.txt | gmt convert -El -o2)
-    line_increment=$(gmt math -Q ${dist_to_Venice} 30 ${MOVIE_RATE} MUL DIV =)
-    gmt sample1d cities.txt -T${line_increment}k+a > tmp_time.txt
+    line_increment=$(gmt math -Q ${dist_to_Venice} ${animation_duration} ${MOVIE_RATE} MUL DIV =)
+    gmt sample1d cities.txt -T${line_increment}k+a > time.txt
 gmt end
 EOF
 
 cat << 'EOF' > main.sh
 gmt begin
-	gmt coast -R-800/800/-450/450+uk -JG${MOVIE_COL0}/${MOVIE_COL1}/24c -Bg0 -Df -G200 -Sdodgerblue2 -N1/0.2,- -Y0p -X0p
-	gmt events tmp_time.txt -Ar -T${MOVIE_COL2} -Es -W2p,red
+	gmt coast -R480/270+uk -JG${MOVIE_COL0}/${MOVIE_COL1}/${MOVIE_WIDTH} -Bg0 -Df -G200 -Sdodgerblue2 -N1/0.2,- -Y0 -X0
+	gmt events time.txt -Ar -T${MOVIE_COL2} -Es -W2p,red
 gmt end
 EOF
 
 #	Create animation
-gmt movie main.sh -Sbpre.sh -N$title -Ttmp_time.txt -Chd -Vi -Ml,png -Fmp4 -Zs
+gmt movie main.sh -Sbpre.sh -N$title -Ttime.txt -Chd -Fmp4 -Zs
