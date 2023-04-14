8f3576326dbec730e30431ac6578f7d79722b26e
Paul Wessel <pwessel@hawaii.edu>
2023-04-14 13:29:33 +0200

    Update script per guidelines
    Also fix length bug and use improved sample1d

--- Federico/1_Jones_Flight/IndianaJones_Flight_NY-Venice.sh
+++ Federico/1_Jones_Flight/IndianaJones_Flight_NY-Venice.sh
@@ -3,27 +3,26 @@ title=IndianaJones_Flight_NY-Venice
 
 cat << 'EOF' > pre.sh
 gmt begin
-gmt set PROJ_ELLIPSOID Sphere
+	gmt set PROJ_ELLIPSOID Sphere
 
-# Escalas del viaje
-cat << 'FILE' > cities.txt
-40.712, -74.007, New York
-47.562, -52.712, St Johns (Terranova)
-37.742, -25.696, Sao Miguel (Azores)
-38.776, -9.135, Lisboa (Portugal)
-45.503,  12.342, Venecia (Italia)
-FILE
-    L=$(gmt mapproject -G+uk cities.txt  --PROJ_ELLIPSOID=Sphere | gmt convert -El -o2)
-    dx=$(gmt math -Q ${L} 30 24 MUL DIV =)
-    gmt sample1d "cities.txt" -I${dx}k -fg -i1,0 | gmt mapproject -G+uk > "tmp_time.txt"
-    gmt events -Rd "tmp_time.txt" -Ar100c > tmp_points.txt
+	# Dr. Jones stopover cities
+	cat <<- 'FILE' > cities.txt
+	-74.007	40.712	New York
+	-52.712	47.562	St Johns (Terranova)
+	-25.696	37.742	Sao Miguel (Azores)
+	-9.135	38.776	Lisboa (Portugal)
+	 12.342	45.503	Venecia (Italia)
+	FILE
+    dist_to_Venice=$(gmt mapproject -G+uk cities.txt | gmt convert -El -o2)
+    line_increment=$(gmt math -Q ${dist_to_Venice} 30 ${MOVIE_RATE} MUL DIV =)
+    gmt sample1d cities.txt -T${line_increment}k+a > tmp_time.txt
 gmt end
 EOF
 
 cat << 'EOF' > main.sh
 gmt begin
-    gmt coast -R-800/800/-450/450+uk -JG${MOVIE_COL0}/${MOVIE_COL1}/24c -Bg0 -Df -G200 -Sdodgerblue2 -N1/0.2,- -Y0p -X0p
-    gmt events tmp_time.txt -Ar -T${MOVIE_COL2} -Es -W2p,red
+	gmt coast -R-800/800/-450/450+uk -JG${MOVIE_COL0}/${MOVIE_COL1}/24c -Bg0 -Df -G200 -Sdodgerblue2 -N1/0.2,- -Y0p -X0p
+	gmt events tmp_time.txt -Ar -T${MOVIE_COL2} -Es -W2p,red
 gmt end
 EOF
 
