e17257bcfc4ffd6fd98a76219f2639b8bdbb6028
Esteban82 <federico.esteban@gmail.com>
2023-04-25 10:11:41 -0300

    Minor modifications.

--- Federico/1a_Jones_Flight_Simple/IndianaJones_Flight_Simple.sh
+++ Federico/1a_Jones_Flight_Simple/IndianaJones_Flight_Simple.sh
@@ -5,11 +5,11 @@ title=IndianaJones_Flight_Simple
 cat << 'EOF' > in.sh
 	# Dr. Jones stopover cities
 	cat <<- 'FILE' > cities.txt
-	-74.007	40.712	New York (USA)
-	-52.712	47.562	St Johns (Terranova)
-	-25.696	37.742	Sao Miguel (Azores)
-	-9.135	38.776	Lisbon (Portugal)
-	 12.342	45.503	Venice (Italy)
+	-74.007	40.712	New York
+	-52.712	47.562	St Johns (Newfoundlad)
+	-25.696	37.742	São Miguel (Azores)
+	-9.135	38.776	Lisbon
+	 12.342	45.503	Venice
 	FILE
 EOF
 
@@ -22,10 +22,10 @@ EOF
 
 cat << 'EOF' > main.sh
 gmt begin
-	gmt coast -R480/270+uk -JG${MOVIE_COL0}/${MOVIE_COL1}/${MOVIE_WIDTH} -Bg0 -Df -G200 -Sdodgerblue2 -N1/0.2,- -Y0 -X0
+	gmt coast -R480/270+uk -JG${MOVIE_COL0}/${MOVIE_COL1}/${MOVIE_WIDTH} -Df -G200 -Sdodgerblue2 -N1/0.2,- -Y0 -X0
 	gmt events distance_vs_frame.txt -Ar -T${MOVIE_COL2} -Es -W3p,red
 gmt end
 EOF
 
 #	Create animation
-gmt movie main.sh -Iin.sh -Sbpre.sh -N${title} -Tdistance_vs_frame.txt -Chd -Vi -H4 -Ml,png -Fmp4 -Zs
+gmt movie main.sh -Iin.sh -Sbpre.sh -N${title} -Tdistance_vs_frame.txt -CHD -Vi -H4 -Ml,png -Fmp4 -Zs
