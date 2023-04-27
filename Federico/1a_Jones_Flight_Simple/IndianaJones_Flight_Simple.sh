ae45c74a8b19287b441b58235e6d25c500bfdd0b
Esteban82-HP <SU-CORREO-ELECTRONICO0>
2023-04-27 19:45:06 -0300

    Fix typo in Newfoundland

--- Federico/1a_Jones_Flight_Simple/IndianaJones_Flight_Simple.sh
+++ Federico/1a_Jones_Flight_Simple/IndianaJones_Flight_Simple.sh
@@ -6,7 +6,7 @@ cat << 'EOF' > in.sh
 	# Dr. Jones stopover cities
 	cat <<- 'FILE' > cities.txt
 	-74.007	40.712	New York
-	-52.712	47.562	St Johns (Newfoundlad)
+	-52.712	47.562	St Johns (Newfoundland)
 	-25.696	37.742	São Miguel (Azores)
 	-9.135	38.776	Lisbon
 	 12.342	45.503	Venice
