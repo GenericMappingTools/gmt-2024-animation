d259901478f45f45b291943800afba149f118540
Esteban82 <federico.esteban@gmail.com>
2023-07-02 17:31:53 -0300

    actualizar

--- Movie_Messi_v2.sh
+++ Movie_Messi_v2.sh
@@ -12,7 +12,7 @@ cat << 'EOF' > in.sh
     REGION2=-11/18/35/54
     #REGION2=ESC # España
     #REGION2=ESC,PTC,FR,GB,DE+R0/5/0/0
-    REGION2=ESC+R40
+    REGION2=ESC+R1
     #REGION=$(gmt info Messi_Goals.txt -i1,2 -I10)
 	PROJ=W7.5
     PROJ2=M8c
@@ -33,11 +33,12 @@ gmt begin
     gmt basemap -R\${REGION} -J\${PROJ}/\${MOVIE_WIDTH} -B+n -Y\${Y} -X0
 
 ##	a. Crear grilla para sombreado a partir del DEM
+    #gmt grdcut @earth_relief
 #	gmt grdgradient @earth_relief_05m -Nt1.2 -A270 -Gtmp_intens.nc
 
 ##	b. Graficar imagen satelital 
 #	gmt grdimage  @earth_day_05m -Itmp_intens.nc
-    gmt grdimage  @earth_day_15m
+    gmt grdimage  @earth_day_20m
 	gmt coast -Df -N1/thinnest
 
 ##	c. Graficar zoom Europa W
@@ -55,15 +56,15 @@ EOF
 cat << EOF > main.sh
 gmt begin
 	gmt basemap -R\${REGION} -J\${PROJ}/\${MOVIE_WIDTH} -B+n -Y\${Y} -X0
-	#gmt events temp_q.txt -SE- -Ctemp_q.cpt --TIME_UNIT=d -T\${MOVIE_COL0} -Es+r6+d18 -Ms2.5+c0.5 -Mi5+c0 -Mt+c0 -Wfaint
+	gmt events temp_q.txt -SE- -Ctemp_q.cpt --TIME_UNIT=d -T\${MOVIE_COL0} -Es+r6+d18 -Ms2.5+c0.5 -Mi5+c0 -Mt+c0 -Wfaint
     gmt basemap -R\${REGION2} -J\${PROJ2} -B+n -X9c -Y\${Y2}
-	gmt events temp_q2.txt -SE- -Ctemp_q.cpt --TIME_UNIT=d -T\${MOVIE_COL0} -Es+r6+d18 -Ms2.5+c0.5 -Mi5+c0 -Mt+c0 -Wfaint
+	#gmt events temp_q2.txt -SE- -Ctemp_q.cpt --TIME_UNIT=d -T\${MOVIE_COL0} -Es+r6+d18 -Ms2.5+c0.5 -Mi5+c0 -Mt+c0 -Wfaint
 gmt end
 EOF
 
 #	----------------------------------------------------------------------------------------------------------
 # 	3. Run the movie
-	gmt movie main.sh -Iin.sh -Sbpre.sh -C24cx10.46cx80 -Ttimes.txt -NMovie_Messi_v2 -H2 -D24 -Ml,png -Vi -Zs -Fnone -Gred \
+	gmt movie main.sh -Iin.sh -Sbpre.sh -C24cx10.475cx80 -Ttimes.txt -NMovie_Messi_v2 -W -H2 -D24 -Ml,png -Vi -Zs -Fmp4 -Gred -K+po \
 	-Lc0+jTR+o0.3/0.3+gwhite+h+r --FONT_TAG=14p,Helvetica,black --FORMAT_CLOCK_MAP=- --FORMAT_DATE_MAP=dd-mm-yyyy       \
 	-Lc1+jTL+o0.3/0.3+gwhite+h+r # -Pf+ac0 #+jRM+w5.9c+o2.7/0.8c+P3,white+p1,red+a1+f11p,2,white
 
