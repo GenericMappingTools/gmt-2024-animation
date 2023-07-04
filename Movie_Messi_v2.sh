dcfa62f3c8938e705151441b23d37e080b21488d
Esteban82 <federico.esteban@gmail.com>
2023-07-04 19:29:45 -0300

    Actualizado

--- Movie_Messi_v2.sh
+++ Movie_Messi_v2.sh
@@ -8,16 +8,16 @@ export http_proxy="http://proxy.fcen.uba.ar:8080"
 cat << 'EOF' > in.sh
 #	Proyeccion del mapa y ancho del mapa
 	REGION=-130/145/-40/64
-    REGION2=-10/25/35/50
-    REGION2=-11/18/35/54
     #REGION2=ESC # España
-    #REGION2=ESC,PTC,FR,GB,DE+R0/5/0/0
-    REGION2=ESC+R1/1/0.2/1
+    #REGION2=ESC+R1/1/0.2/1
+    REGION2=PTC,ESC,FR,GB,DE+R1/3/1/-3.5
+    
     #REGION=$(gmt info Messi_Goals.txt -i1,2 -I10)
 	PROJ=W7.5
-    PROJ2=M7.5c
+    PROJ2=M5.5c
     Y=0c
-    Y2=5p
+    Y2=3.168p
+    X=8.5c
     #gmt get MAP_FRAME_WIDTH
 EOF
 
@@ -34,17 +34,19 @@ gmt begin
 
 ##	a. Crear grilla para sombreado a partir del DEM
     #gmt grdcut @earth_relief
-#	gmt grdgradient @earth_relief_05m -Nt1.2 -A270 -Gtmp_intens.nc
+	gmt grdgradient @earth_relief_05m -Nt1.2 -A270 -Gtmp_intens.nc
 
 ##	b. Graficar imagen satelital 
-#	gmt grdimage  @earth_day_05m -Itmp_intens.nc
-    gmt grdimage  @earth_day_20m
+	gmt grdimage  @earth_day_05m -Itmp_intens.nc
+#   gmt grdimage  @earth_day
 	gmt coast -Df -N1/thinnest
 
 ##	c. Graficar zoom Europa W
-    gmt get MAP_FRAME_WIDTH
+    #gmt get MAP_FRAME_WIDTH
 
-    gmt grdimage  @earth_day_03m -R\${REGION2} -J\${PROJ2} -X9c -Y\${Y2} -Bf
+	gmt grdgradient @earth_relief_05m -Nt1.2 -A270 -Gtmp_intens2.nc
+    gmt grdimage  @earth_day -Itmp_intens.nc -R\${REGION2} -J\${PROJ2} -X\${X} -Y\${Y2} -Bf
+    #gmt grdimage  @earth_day -R\${REGION2} -J\${PROJ2} -X\${X} -Y\${Y2} -Bf
     gmt coast -Df -N1/thinnest
 
     gmt makecpt -Chot -T1/7/1 -I -H > temp_q.cpt
@@ -59,17 +61,16 @@ cat << EOF > main.sh
 gmt begin
 	gmt basemap -R\${REGION} -J\${PROJ}/\${MOVIE_WIDTH} -B+n -Y\${Y} -X0
 	gmt events temp_q.txt -SE- -Ctemp_q.cpt --TIME_UNIT=d -T\${MOVIE_COL0} -Es+r6+d18 -Ms2.5+c0.5 -Mi5+c0 -Mt+c0 -Wfaint
-    gmt basemap -R\${REGION2} -J\${PROJ2} -B+n -X9c -Y\${Y2}
+    gmt basemap -R\${REGION2} -J\${PROJ2} -B+n -X\${X} -Y\${Y2}
 	gmt events temp_q2.txt -SE- -Ctemp_q.cpt --TIME_UNIT=d -T\${MOVIE_COL0} -Es+r6+d18 -Ms2.5+c0.5 -Mi5+c0 -Mt+c0 -Wfaint
 gmt end
 EOF
 
 #	----------------------------------------------------------------------------------------------------------
 # 	3. Run the movie
-	#gmt movie main.sh -Iin.sh -Sbpre.sh -C24cx10.46cx80 -Ttimes.txt -NMovie_Messi_v2 -W -H2 -D24 -Ml,png -Vi -Zs -Gred -K+po \
-    gmt movie main.sh -Iin.sh -Sbpre.sh -Cfhd -Ttimes.txt -NMovie_Messi_v2 -W -H2 -D24 -Ml,png -Vi -Zs -Gblack -K+po \
-	-Lc0+jTR+o0.3/0.3+gwhite+h+r --FONT_TAG=14p,Helvetica,black --FORMAT_CLOCK_MAP=- --FORMAT_DATE_MAP=dd-mm-yyyy       \
-	-Lc1+jTL+o0.3/0.3+gwhite+h+r #  -Fmp4 -Pf+ac0 #+jRM+w5.9c+o2.7/0.8c+P3,white+p1,red+a1+f11p,2,white
+	gmt movie main.sh -Iin.sh -Sbpre.sh -C24cx10.46cx80 -Ttimes.txt -NMovie_Messi_v2 -W -H2 -D24 -Ml,png -Vi -Zs -Gblack  \
+    -Lc0+jTR+o0.3/0.3+gwhite+h+r --FONT_TAG=14p,Helvetica,black --FORMAT_CLOCK_MAP=- --FORMAT_DATE_MAP=dd-mm-yyyy       \
+	-Lc1+jTL+o0.3/0.3+gwhite+h+r -Fmp4 #-Pf+ac0 #+jRM+w5.9c+o2.7/0.8c+P3,white+p1,red+a1+f11p,2,white -K+po
 
 # Place animation
 mkdir -p mp4
