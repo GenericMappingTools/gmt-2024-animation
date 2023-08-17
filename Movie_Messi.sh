3782d219d8c8f0fb282786e18641e1640793de77
Esteban82-HP <SU-CORREO-ELECTRONICO0>
2023-08-17 08:33:51 -0300

    improve comments and translate

--- Movie_Messi.sh
+++ Movie_Messi.sh
@@ -24,39 +24,39 @@ EOF
 cat << EOF > pre.sh
 gmt begin
 
-# 3a. Create files for animation.
+# 3A. Create files for animation
 #	1. Reorder and scale data:
 	gmt convert Messi_Goals.txt -i1,2,3,3+s400,0 > data_scale_by_400.txt
     gmt convert Messi_Goals.txt -i1,2,3,3+s80,0  > data_scale_by_80.txt
 
-#   2. Create file with dates and accumulative sum for the labels
-    gmt math Messi_Goals.txt -C3 SUM -o0,3 = | gmt sample1d $(gmt info Messi_Goals.txt -T3d) -Fe -fT > times.txt
+#   2. Create file with dates versus accumulative sum of goals
+    gmt math Messi_Goals.txt -C3 SUM -o0,3 = | gmt sample1d $(gmt info Messi_Goals.txt -T3d) -Fe -fT > dates_vs_goals.txt
 
-# 3b. Make statics background maps
+# 3B. Make statics background maps
 #   1. Plot main map
     gmt basemap -R${main_map_region} -J${main_map_projection}/\${MOVIE_WIDTH} -B+n -Y0 -X0
 
 #	a. Create intesity grid for shadow effect
-	gmt grdgradient @earth_relief_05m_p -Nt1.2 -A270 -Gtmp_intens.nc
+	gmt grdgradient @earth_relief_05m_p -Nt1.2 -A270 -Gmain_intensity.nc -R${main_map_region}
 
 #	b. Plot satellite image with shadow effect and coastlines
-    gmt grdimage  @earth_day_05m -Itmp_intens.nc
+    gmt grdimage  @earth_day_05m -Imain_intensity.nc
     gmt coast -N1/thinnest #-Df
 
 #   c. Create and draw CPT
     gmt makecpt \$(gmt info Messi_Goals.txt -T1+c3) -Chot -I -F+c1 -H > Goals.cpt
 
-    # Plot colorbar near the bottom lefth of the canvas with a background panel.
+    # Plot colorbar near the bottom left of the canvas with a background panel.
     gmt colorbar -CGoals.cpt -DjBL+o0.7c/0.5c+w50% -F+gwhite+p+i+s2p/-2p -L0.1 -S+y"Goals"
 
-#   d. Draw rectangular polygone showing the area of the inset map in the main map
+#   d. Draw a rectangle showing the area of the inset map
 	gmt basemap -R\${inset_map_region} -J\${inset_map_projection} -A | gmt plot -Wthick,white
 
 #	e. Plot inset map with zoom in western Europe
     gmt inset begin -Dx\${X}/\${Y} -F+p+s -R\${inset_map_region} -J\${inset_map_projection}
-        gmt grdgradient @earth_relief_01m_p -Nt1.2 -A270 -Gtmp_intens2.nc -R\${inset_map_region}
-        gmt grdimage  @earth_day -Itmp_intens2.nc
-        gmt coast -Df -N1/thinnest -Bf --MAP_FRAME_TYPE=plain --MAP_FRAME_PEN=white
+        gmt grdgradient @earth_relief_01m_p -Nt1.2 -A270 -Ginset_intensity.nc -R\${inset_map_region}
+        gmt grdimage  @earth_day -Iinset_intensity.nc
+        gmt coast -N1/thinnest -Bf --MAP_FRAME_TYPE=plain --MAP_FRAME_PEN=white #-Df
     gmt inset end
 
 gmt end
@@ -75,9 +75,9 @@ EOF
 
 #	----------------------------------------------------------------------------------------------------------
 # 	5. Run the movie
-gmt movie main.sh -Iin.sh -Sbpre.sh -C${canvas_width}cx${canvas_heigth}cx80 -Ttimes.txt -NMovie_Messi -H2 -D24 -Ml,png -Vi -Zs -Gblack \
+gmt movie main.sh -Iin.sh -Sbpre.sh -C${canvas_width}cx${canvas_height}cx80 -Tdates_vs_goals.txt -NMovie_Messi -H2 -Ml,png -Vi -Zs -Gblack \
     -Lc0+jTR+o0.3/0.3+gwhite+h2p/-2p+r --FONT_TAG=14p,Courier-Bold,black --FORMAT_CLOCK_MAP=- --FORMAT_DATE_MAP=dd-mm-yyyy   \
-	-Lc1+jTL+o0.3/0.3+gwhite+h2p/-2p+r # -Fmp4
+	-Lc1+jTL+o0.3/0.3+gwhite+h2p/-2p+r # -Fmp4 -D24
 
 # Place animation
 mkdir -p mp4
