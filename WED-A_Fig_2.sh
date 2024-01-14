cc56a5ebbec5eac8737776b56f1d353c60f7c0fa
Paul Wessel <pwessel@hawaii.edu>
2024-01-14 17:58:27 +0100

    cleaning

--- WED-A_Fig_2.sh
+++ WED-A_Fig_2.sh
@@ -12,13 +12,12 @@
 FIG=WED-A_Fig_2
 
 # Make dummy map script for the static "movie"
-cat << EOF > map.sh
+cat << EOF > main.sh
 gmt begin
 	gmt set GMT_THEME cookbook
 	gmt basemap -R0/10/0/5 -JX20c/8.5c -Bafg -BWSrt+glightgray
 gmt end show
 EOF
 
-gmt movie map.sh -CHD -T50 -M10,png -N${FIG} -Pa+jTL -Pb+jTC+ap -Pc+ap -Pd+jLM+ap -Pe+ap+jRM -Pf+ap -W/tmp/junk -Zs \
+gmt movie main.sh -CHD -T50 -M10,png -N${FIG} -Pa+jTL -Pb+jTC+ap -Pc+ap -Pd+jLM+ap -Pe+ap+jRM -Pf+ap -W/tmp/junk -Zs \
 	-Ls"a)"+jTL+o1.7c/0.5c -Ls"b)"+jTC+o-1.1c/0.5c -Ls"c)"+jTR+o1.7c/0.5c -Ls"d)"+jML+o0.7c/0  -Ls"e)"+jMR+o0.6c/0 -Ls"f)"+jBC+o-8c/0.5c
-rm -f map.sh
